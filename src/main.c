aa02cba9d414a5252c55b34c362ed71e9e37a947
ouer <ouer1224@qq.com>
2020-03-27 11:09:06 +0800

    后续的版本用于测试上下文的切换

--- src/main.c
+++ src/main.c
@@ -23,8 +23,8 @@ void PORT_init (void) {
   PORTE->PCR[4] |= PORT_PCR_MUX(5); /* Port E4: MUX = ALT5, CAN0_RX */
   PORTE->PCR[5] |= PORT_PCR_MUX(5); /* Port E5: MUX = ALT5, CAN0_TX */
   PCC->PCCn[PCC_PORTD_INDEX ]|=PCC_PCCn_CGC_MASK;   /* Enable clock for PORTD */
-  PORTD->PCR[16] =  0x00000100;     /* Port D16: MUX = GPIO (to green LED) */
-  PTD->PDDR |= 1<<16;               /* Port D16: Data direction = output */
+  PORTD->PCR[15] =  0x00000100;     /* Port D16: MUX = GPIO (to green LED) */
+  PTD->PDDR |= 1<<15;               /* Port D16: Data direction = output */
 }
 void Dlyms(int tick) {
     int i,j,k=0;
@@ -35,6 +35,26 @@ void Dlyms(int tick) {
         }
     }
 }
+#define __SRAM_BASE_ADDR		0x1FFF8000
+#define __SRAM_SIZE				0x0000F000
+
+#define STACK_SIZE  0x400
+void CleanRAM(void)
+{
+    uint32_t cnt;
+    uint32_t *pRam;
+
+    /*
+     * clean system SRAM
+     */
+    cnt = (__SRAM_SIZE - STACK_SIZE)/4;
+    pRam = (uint32_t*)__SRAM_BASE_ADDR;
+
+    while(cnt--)
+        *pRam++ = 0;
+
+}
+
 int main(void) {
   uint32_t rx_msg_count = 0;
 
@@ -43,25 +63,27 @@ int main(void) {
   SPLL_init_160MHz();     /* Initialize SPLL to 160 MHz with 8 MHz SOSC */
   NormalRUNmode_80MHz();  /* Init clocks: 80 MHz sysclk & core, 40 MHz bus, 20 MHz flash */
 
-  FLEXCAN0_init();         /* Init FlexCAN0 */
   PORT_init();             /* Configure ports */
-#ifdef NODE_A              /* Node A transmits first; Node B transmits after reception */
-  FLEXCAN0_transmit_msg(); /* Transmit initial message from Node A to Node B */
-#endif
 
   for (;;) {                        /* Loop: if a msg is received, transmit a msg */
 //    if ((CAN0->IFLAG1 >> 4) & 1)
-	  if(1)
+	  rx_msg_count++;
+	  if(rx_msg_count%2==0)
 	  {  /* If CAN 0 MB 4 flag is set (received msg), read MB4 */
-      FLEXCAN0_receive_msg ();      /* Read message */
-      rx_msg_count++;               /* Increment receive msg counter */
+           /* Increment receive mug counter */
 
-      if (rx_msg_count == 100) {   /* If 1000 messages have been received, */
-        PTD->PTOR |= 1<<16;         /*   toggle output port D16 (Green LED) */
-        rx_msg_count = 0;           /*   and reset message counter */
-      }
-      Dlyms(1000);
-      FLEXCAN0_transmit_msg ();     /* Transmit message using MB0 */
-    }
+        PTD->PTOR |= 1<<15;         /*   toggle output port D16 (Green LED) */
+
+        Dlyms(20000);
+
+	  }
+	  else
+	  {
+
+		  PTD->PSOR |= 1<<15;
+
+		  Dlyms(20000);
+	  }
+
   }
 }
